a2bcfba76338f7da5b24b8617e56cdf406793dc9
LMH-ironman <970259067@qq.com>
2023-08-11 16:42:38 +0800

    first

---
+++ (3)差.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+#include <iostream>
+
+int main()
+{
+    int a, b, c, d;
+    scanf("%d\n %d\n %d\n %d\n", &a, &b, &c, &d);
+    printf("DIFERENCA = %d", a * b - c * d);
+    return 0;
+}
